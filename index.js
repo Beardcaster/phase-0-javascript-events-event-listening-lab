63de0fee308b2fef43dc44270999abc52d519b6c
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-07 20:49:34 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const btn = document.getElementById("button")
+    btn.addEventListener('click', () => {
+
+        console.log("click")
+        alert("I was clicked");
+    })
+
+
 }
+addingEventListener();
